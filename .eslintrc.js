3e115a6fb46fc62c0269bb765808bd938388afbe
zorzysty <zorza2@gmail.com>
2019-07-05 10:48:45 +0200

    Major upgrade react-scripts dependency to fix vulnerabilities found by npm audit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,6 @@
 module.exports = {
   "extends": ["airbnb", "react-app"],
   "rules": {
-    "import/extensions": { "js": "never" },
     "linebreak-style": "off",
     "jsx-a11y/label-has-associated-control": "off",
     "react/jsx-filename-extension": "off",
